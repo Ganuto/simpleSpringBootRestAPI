b603429bb51b5e546da438806ae037179b927d84
Canuto <ganuto@hotmail.com>
2020-11-09 19:15:08 -0300

    Adding a small dependency

--- src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldController.java
+++ src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldController.java
@@ -2,17 +2,20 @@ package com.example.simpleRestAPI.helloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import simpleLib.Library;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
+        Library library = new Library();
+
         return new HelloWorldBean("Hello World");
     }
 
