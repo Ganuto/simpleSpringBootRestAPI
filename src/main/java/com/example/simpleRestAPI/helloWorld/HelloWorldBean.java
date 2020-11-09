a1b16ba924a1a89f48ae23176c68cff333350904
Canuto <ganuto@hotmail.com>
2020-11-09 19:41:36 -0300

    upgrading dependency; creating a get using the jitLibTest dependency

--- src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldBean.java
+++ src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldBean.java
@@ -2,9 +2,14 @@ package com.example.simpleRestAPI.helloWorld;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import simpleLib.annotations.JsonElement;
+import simpleLib.annotations.JsonSerializable;
 
 @AllArgsConstructor
 @Getter
+@JsonSerializable
 public class HelloWorldBean {
+
+    @JsonElement
     private String message;
 }
--- src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldController.java
+++ src/main/java/com/example/simpleRestAPI/helloWorld/HelloWorldController.java
@@ -2,7 +2,8 @@ package com.example.simpleRestAPI.helloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import simpleLib.Library;
+import simpleLib.ObjectToJsonConverter;
+import simpleLib.annotations.JsonSerializable;
 
 @RestController
 public class HelloWorldController {
@@ -14,9 +15,15 @@ public class HelloWorldController {
 
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        Library library = new Library();
-
         return new HelloWorldBean("Hello World");
     }
 
+    @GetMapping("/hello-world-bean-lib")
+    public String helloWorldWithLibReturn() throws Exception {
+        HelloWorldBean helloWorldBean = new HelloWorldBean("sapato");
+        ObjectToJsonConverter objectToJsonConverter = new ObjectToJsonConverter();
+        String response = objectToJsonConverter.convertToJson(helloWorldBean);
+
+        return response;
+    }
 }
